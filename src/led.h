b7f4b629625fcf118fb5acb2a03f89387ae14314
swensong <wen_emil@163.com>
2019-02-27 00:06:53 +0800

    led PC13 可以使用

--- src/led.h
+++ src/led.h
@@ -1,24 +1,54 @@
 #ifndef __LED_H
 #define __LED_H	 
-#include "sys.h"
-//////////////////////////////////////////////////////////////////////////////////	 
-//本程序只供学习使用，未经作者许可，不得用于其它任何用途
-//ALIENTEK战舰STM32开发板V3
-//LED驱动代码	   
-//正点原子@ALIENTEK
-//技术论坛:www.openedv.com
-//创建日期:2015/1/9
-//版本：V1.0
-//版权所有，盗版必究。
-//Copyright(C) 广州市星翼电子科技有限公司 2009-2019
-//All rights reserved									  
-////////////////////////////////////////////////////////////////////////////////// 
+
+#include "stm32f10x.h"
+
+#define BITBAND(addr, bitnum) ((addr & 0xF0000000)+0x2000000+((addr &0xFFFFF)<<5)+(bitnum<<2))
+#define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr))
+#define BIT_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum))
+
+#define GPIOA_ODR_Addr    (GPIOA_BASE+12) //0x4001080C
+#define GPIOB_ODR_Addr    (GPIOB_BASE+12) //0x40010C0C
+#define GPIOC_ODR_Addr    (GPIOC_BASE+12) //0x4001100C
+#define GPIOD_ODR_Addr    (GPIOD_BASE+12) //0x4001140C
+#define GPIOE_ODR_Addr    (GPIOE_BASE+12) //0x4001180C
+#define GPIOF_ODR_Addr    (GPIOF_BASE+12) //0x40011A0C
+#define GPIOG_ODR_Addr    (GPIOG_BASE+12) //0x40011E0C
+
+#define GPIOA_IDR_Addr    (GPIOA_BASE+8) //0x40010808
+#define GPIOB_IDR_Addr    (GPIOB_BASE+8) //0x40010C08
+#define GPIOC_IDR_Addr    (GPIOC_BASE+8) //0x40011008
+#define GPIOD_IDR_Addr    (GPIOD_BASE+8) //0x40011408
+#define GPIOE_IDR_Addr    (GPIOE_BASE+8) //0x40011808
+#define GPIOF_IDR_Addr    (GPIOF_BASE+8) //0x40011A08
+#define GPIOG_IDR_Addr    (GPIOG_BASE+8) //0x40011E08
+
+#define PAout(n)   BIT_ADDR(GPIOA_ODR_Addr,n)
+#define PAin(n)    BIT_ADDR(GPIOA_IDR_Addr,n)
+
+#define PBout(n)   BIT_ADDR(GPIOB_ODR_Addr,n)
+#define PBin(n)    BIT_ADDR(GPIOB_IDR_Addr,n)
+
+#define PCout(n)   BIT_ADDR(GPIOC_ODR_Addr,n)
+#define PCin(n)    BIT_ADDR(GPIOC_IDR_Addr,n)
+
+#define PDout(n)   BIT_ADDR(GPIOD_ODR_Addr,n)
+#define PDin(n)    BIT_ADDR(GPIOD_IDR_Addr,n)
+
+#define PEout(n)   BIT_ADDR(GPIOE_ODR_Addr,n)
+#define PEin(n)    BIT_ADDR(GPIOE_IDR_Addr,n)
+
+#define PFout(n)   BIT_ADDR(GPIOF_ODR_Addr,n)
+#define PFin(n)    BIT_ADDR(GPIOF_IDR_Addr,n)
+
+#define PGout(n)   BIT_ADDR(GPIOG_ODR_Addr,n)
+#define PGin(n)    BIT_ADDR(GPIOG_IDR_Addr,n)
 
 //LED端口定义
-#define LED0 PBout(5)	// DS0
-#define LED1 PEout(5)	// DS1	
+#define LED0 PCout(13)	//LED
 
-void LED_Init(void);	//初始化		 				    
+void LED_Init(void);
+
 #endif
 
 
--- src/main.c
+++ src/main.c
@@ -1,24 +1,70 @@
-#include "sys.h" 	
-#include "delay.h"	
 #include "led.h"
-//ALIENTEK战舰STM32开发板 实验1
-//跑马灯 实验  
-//技术支持：www.openedv.com
-//广州市星翼电子科技有限公司
+
+//设置向量表偏移地址
+//NVIC_VectTab:基址
+//Offset:偏移量			 
+void MY_NVIC_SetVectorTable(u32 NVIC_VectTab, u32 Offset)	 
+{ 	   	 
+	SCB->VTOR = NVIC_VectTab|(Offset & (u32)0x1FFFFF80);//设置NVIC的向量表偏移寄存器
+	//用于标识向量表是在CODE区还是在RAM区
+}
+//不能在这里执行所有外设复位!否则至少引起串口不工作.		    
+//把所有时钟寄存器复位		  
+void MYRCC_DeInit(void)
+{	
+ 	RCC->APB1RSTR = 0x00000000;//复位结束			 
+	RCC->APB2RSTR = 0x00000000; 
+	  
+  	RCC->AHBENR = 0x00000014;  //睡眠模式闪存和SRAM时钟使能.其他关闭.	  
+  	RCC->APB2ENR = 0x00000000; //外设时钟关闭.			   
+  	RCC->APB1ENR = 0x00000000;   
+	RCC->CR |= 0x00000001;     //使能内部高速时钟HSION	 															 
+	RCC->CFGR &= 0xF8FF0000;   //复位SW[1:0],HPRE[3:0],PPRE1[2:0],PPRE2[2:0],ADCPRE[1:0],MCO[2:0]					 
+	RCC->CR &= 0xFEF6FFFF;     //复位HSEON,CSSON,PLLON
+	RCC->CR &= 0xFFFBFFFF;     //复位HSEBYP	   	  
+	RCC->CFGR &= 0xFF80FFFF;   //复位PLLSRC, PLLXTPRE, PLLMUL[3:0] and USBPRE 
+	RCC->CIR = 0x00000000;     //关闭所有中断		 
+	//配置向量表				  
+	/* MY_NVIC_SetVectorTable(0x20000000, 0x0); */
+	MY_NVIC_SetVectorTable(0x08000000,0x0);
+}
+
+void SystemInit(u8 PLL)
+{
+	unsigned char temp=0;   
+	MYRCC_DeInit();		  //复位并配置向量表
+ 	RCC->CR|=0x00010000;  //外部高速时钟使能HSEON
+	while(!(RCC->CR>>17));//等待外部时钟就绪
+	RCC->CFGR=0X00000400; //APB1=DIV2;APB2=DIV1;AHB=DIV1;
+	PLL-=2;				  //抵消2个单位（因为是从2开始的，设置0就是2）
+	RCC->CFGR|=PLL<<18;   //设置PLL值 2~16
+	RCC->CFGR|=1<<16;	  //PLLSRC ON 
+	FLASH->ACR|=0x32;	  //FLASH 2个延时周期
+	RCC->CR|=0x01000000;  //PLLON
+	while(!(RCC->CR>>25));//等待PLL锁定
+	RCC->CFGR|=0x00000002;//PLL作为系统时钟	 
+	while(temp!=0x02)     //等待PLL作为系统时钟设置成功
+	{   
+		temp=RCC->CFGR>>2;
+		temp&=0x03;
+	}    
+}		    
+
+
 int main(void)
 {				  
+    u32 counter = 0;
 
-	SystemInit(9);//系统时钟设置
-	delay_init(72);	  	//延时初始化
-	LED_Init();		  	//初始化与LED连接的硬件接口
-	while(1)
-	{
-		LED0=0;
-		LED1=1;
-		delay_ms(300);
-		LED0=1;
-		LED1=0;
-		delay_ms(300);
-	}	 
+    SystemInit(9);//系统时钟设置
+    LED_Init();		  	//初始化与LED连接的硬件接口
+    while(1)
+    {
+        /* LED0=0; */
+        GPIOC->BSRR = 0x30000000;
+        for (counter = 0; counter < 0xFFFFF; counter++);
+        /* LED0=1; */
+        GPIOC->BSRR = 0x00003000;
+        for (counter = 0; counter < 0xFFFFF; counter++);
+    }	 
 }
 
